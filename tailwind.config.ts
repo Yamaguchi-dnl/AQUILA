7dec72f33e9601779dac8b5eecfb7f954d979cd5
Daniel Yamaguchi <danielyamaguchi409@gmail.com>
2025-08-27 15:53:03 +0000

    ja coloquei o arquivo da font la agora atualize o codigo para usar aquel

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,7 +18,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['Inter', 'sans-serif'],
-        headline: ['Italiana', 'serif'],
+        headline: ['Austin', 'serif'],
         code: ['monospace'],
       },
       colors: {
