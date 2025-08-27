dcd14ff79085e3d57024ed96e71e06c97c441b22
Daniel Yamaguchi <danielyamaguchi409@gmail.com>
2025-08-27 18:49:51 +0000

    pode prosseguir

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,20 +7,37 @@ import { InvestmentCycle } from '@/components/sections/investment-cycle';
 import { InvestmentStrategy } from '@/components/sections/investment-strategy';
 import { ContactSummary } from '@/components/sections/contact-summary';
 import { StatsCard } from '@/components/sections/stats-card';
+import { AnimatedSection } from '@/components/shared/animated-section';
 
 export default function Home() {
   return (
     <>
       <Hero />
       <div className="relative z-10">
-        <StatsCard />
-        <AboutSummary />
-        <FundsSummary />
-        <WhyPortugal />
-        <GoldenVisaSummary />
-        <InvestmentCycle />
-        <InvestmentStrategy />
-        <ContactSummary />
+        <AnimatedSection>
+          <StatsCard />
+        </AnimatedSection>
+        <AnimatedSection>
+          <AboutSummary />
+        </AnimatedSection>
+        <AnimatedSection>
+          <FundsSummary />
+        </AnimatedSection>
+        <AnimatedSection>
+          <WhyPortugal />
+        </AnimatedSection>
+        <AnimatedSection>
+          <GoldenVisaSummary />
+        </AnimatedSection>
+        <AnimatedSection>
+          <InvestmentCycle />
+        </AnimatedSection>
+        <AnimatedSection>
+          <InvestmentStrategy />
+        </AnimatedSection>
+        <AnimatedSection>
+          <ContactSummary />
+        </AnimatedSection>
       </div>
     </>
   );
