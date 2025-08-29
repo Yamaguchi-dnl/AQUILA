c95399655466b5e9ac1f8f8f62684d19e6ac99d1
Daniel Yamaguchi <danielyamaguchi409@gmail.com>
2025-08-29 16:36:19 +0000

    ainda não está igual

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,16 @@ import { FundsSummary } from '@/components/sections/funds-summary';
 import { WhyPortugal } from '@/components/sections/why-portugal';
 import { GoldenVisaSummary } from '@/components/sections/golden-visa-summary';
 import { InvestmentCycle } from '@/components/sections/investment-cycle';
-import { InvestmentStrategy } from '@/components/sections/investment-strategy';
 import { ContactSummary } from '@/components/sections/contact-summary';
-import { Hero } from '@/components/sections/hero';
 
 export default function Home() {
   return (
     <>
-      <Hero />
       <AboutSummary />
       <FundsSummary />
       <WhyPortugal />
       <GoldenVisaSummary />
       <InvestmentCycle />
-      <InvestmentStrategy />
       <ContactSummary />
     </>
   );
