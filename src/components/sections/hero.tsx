1a3216b0630e238817043a57db05acab55409f07
Daniel Yamaguchi <danielyamaguchi409@gmail.com>
2025-09-02 17:54:47 +0000

    diminua mais ainda o padding

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -8,7 +8,7 @@ import Image from 'next/image';
 export function Hero() {
   return (
     <section className="w-full bg-card z-10">
-      <div className="container z-10 grid md:grid-cols-2 gap-8 items-center pt-40 pb-16">
+      <div className="container z-10 grid md:grid-cols-2 gap-8 items-center pt-32 pb-16">
         <div className="max-w-2xl order-last md:order-first">
             <AnimatedSection delay={0.1}>
                 <h1 className="font-headline text-3xl sm:text-4xl lg:text-5xl xl:text-6xl tracking-tight text-foreground uppercase">
