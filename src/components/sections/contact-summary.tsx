a2d616571c53ce6015992a22bdf036eddc95987e
Daniel Yamaguchi <danielyamaguchi409@gmail.com>
2025-09-03 03:12:28 +0000

    ainda está errado o espaçamento na versão mobile ta muito perto o ultimo

--- src/components/sections/contact-summary.tsx
+++ src/components/sections/contact-summary.tsx
@@ -7,7 +7,7 @@ import { AnimatedSection } from "../shared/animated-section";
 export function ContactSummary() {
   return (
     <section id="contato" className="bg-card rounded-t-3xl -mt-16 relative z-10 shadow-2xl">
-      <div className="container pt-0 pb-16 md:py-24 lg:py-28">
+      <div className="container pt-16 pb-16 md:py-24 lg:py-28">
         <div className="grid lg:grid-cols-2 gap-16 items-center">
             <div>
                 <AnimatedSection>
